10f6bc096f712dc6ffa6923df89395e8f9b0d40a
Jeremy Quentin <jeremy.quentin@gmail.com>
2017-02-02 22:23:37 +0100

    Initial code

---
+++ Assets/Scripts/ColorBeing.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorBeing : MonoBehaviour {
+
+	public const float MAX_HUE_DIF_TO_HEAL = 0.15f;
+	/// <summary>
+	/// The ratio between the max health that can be affected to heal if perfect color match
+	/// and the max health that can be affected to damage if worst color match
+	/// </summary>
+	public const float HEAL_FACTOR = 0.2f;
+
+	public Color color;
+
+	float hue
+	{
+		get
+		{
+			return color.GetHue();
+		}
+	}
+
+	public int maxHealth = 10;
+	int health;
+
+	void Awake()
+	{
+		health = maxHealth;
+	}
+
+	void Update()
+	{
+		BeShot(Color.blue, 10f);
+	}
+
+	public void BeShot (Color c, float force) 
+	{
+		float hue = c.GetHue();
+		float hueDif = ColorUtils.GetHueDif(this.hue, hue);
+		Debug.LogFormat("hue dif = {0}", hueDif);
+		float damage = CalculateDamage(hueDif, force);
+		Debug.LogFormat("shot by damage = {0}", damage);
+	}
+
+	public static float CalculateDamage(float hueDif, float force)
+	{
+//		float multiplier = (hueDif - MAX_HUE_DIF_TO_HEAL) * (1f / (0.5f - MAX_HUE_DIF_TO_HEAL));
+		float multiplier = hueDif - MAX_HUE_DIF_TO_HEAL;
+		if (multiplier < 0f)
+			multiplier *= (1f / MAX_HUE_DIF_TO_HEAL) * HEAL_FACTOR;
+		else
+			multiplier *= (1f / (0.5f - MAX_HUE_DIF_TO_HEAL));
+		return force * multiplier;
+	}
+
+}
---
+++ Assets/Scripts/ColorUtils.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ColorUtils {
+
+	public static float GetHue(this Color color)
+	{
+		float h, s, v;
+		Color.RGBToHSV(color, out h, out s, out v);
+		return h;
+	}
+
+	public static float GetSaturation(this Color color)
+	{
+		float h, s, v;
+		Color.RGBToHSV(color, out h, out s, out v);
+		return s;
+	}
+
+	public static float GetV(this Color color)
+	{
+		float h, s, v;
+		Color.RGBToHSV(color, out h, out s, out v);
+		return v;
+	}
+
+	public static float GetHueDif(float hue1, float hue2)
+	{
+		float hueDif = (hue1 - hue2) % 1f;
+		if (hueDif < 0f) hueDif += 1f;
+		if (hueDif > 0.5f) hueDif = 1f - hueDif;
+		return hueDif;
+	}
+
+	public static float GetHueDif(Color c1, Color c2)
+	{
+		float h1 = c1.GetHue();
+		float h2 = c2.GetHue();
+		return GetHueDif(h1, h2);
+	}
+
+}

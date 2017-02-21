0585c4266d19e798221a9ff9b0aa1c6e0275bda3
Jeremy Quentin <jeremy.quentin@gmail.com>
2017-02-21 19:06:56 +0100

    Add bomb

---
+++ Assets/Scripts/Weapons/Bomb.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : ColorObject {
+
+	public float force = 1f;
+
+	public float timeBeforeExplosion = 2f;
+	public float explodeRadius = 1f;
+
+	public AudioClip fuseSnd;
+
+	public Explosion explosionPrefab;
+
+	float timeDrop;
+
+	void Start () 
+	{
+		timeDrop = Time.time;
+		audioSource.loop = true;
+		audioSource.clip = fuseSnd;
+		audioSource.PlayControlled(AudioType.Sound);
+	}
+
+	void Update () 
+	{
+		if (!isServer)
+			return;
+		if (Time.time >= timeDrop + timeBeforeExplosion)
+		{
+			Explode();
+		}
+	}
+
+
+	void Explode()
+	{
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explodeRadius);
+		Explosion.CreateExplosion(explosionPrefab, transform.position, explodeRadius, color);
+		foreach(Collider2D collider in colliders)
+		{
+			Shootable shootable = collider.GetComponentInParent<Shootable>();
+			if (shootable != null)
+			{
+				shootable.BeShot(color, force);
+			}
+		}
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Weapons/BombDropper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BombDropper : Weapon {
+
+	float timeLastDrop;
+
+	public Bomb bombPrefab;
+
+	protected override void CmdShoot ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	[Command]
+	protected void CmdDropBomb ()
+	{
+		Bomb bomb = Instantiate(bombPrefab, transform.position, Quaternion.identity);
+		bomb.color = this.color;
+		NetworkServer.Spawn(bomb.gameObject);
+	}
+
+	protected virtual void Update()
+	{
+		if (!isLocalPlayer || owner.isDead)
+			return;
+		if (Input.GetMouseButtonDown(0))
+		{
+			CmdDropBomb();
+		}
+	}
+}

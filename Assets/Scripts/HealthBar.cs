374300dbbc48c32d36a4db72636631dfbbaebf68
Jeremy Quentin <jeremy.quentin@gmail.com>
2017-02-26 23:11:27 +0100

    Added health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+	public Transform foreground;
+
+	public void UpdateBar (float health) 
+	{
+		foreground.localScale = new Vector3(1f, health, 1f);
+	}
+}

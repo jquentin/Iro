845d5a73409176b2f5194b3a6dc6dc148e0b2998
Jeremy Quentin <jeremy.quentin@gmail.com>
2017-04-09 11:48:25 +0200

    Integrated AstarPathfinding

--- Assets/Scripts/AI/FollowTarget.cs
+++ Assets/Scripts/AI/FollowTarget.cs
@@ -16,19 +16,58 @@ public class FollowTarget : IroCharacterAction {
 
 	public SharedFloat speed;
 
+	public SharedFloat updatePathDelay;
+
+	Pathfinding.Path currentPath;
+	int currentPathIndex;
+	bool isCalculatingPath = false;
+
+	float lastUpdatePath;
+
 	public override TaskStatus OnUpdate ()
 	{
 		Debug.Log(targetGO.Value);
 		if (targetGO.Value != null)
 		{
-			Vector2 dif = targetGO.Value.transform.position - this.transform.position;
-			float distance = dif.magnitude;
-			if (distance < minDistance.Value)
-				controller.velocity = -(targetGO.Value.transform.position - this.transform.position).normalized * speed.Value;
-			else if (distance > maxDistance.Value)
-				controller.velocity = (targetGO.Value.transform.position - this.transform.position).normalized * speed.Value;
-			else
-				controller.velocity = Vector2.zero;
+			float distance = (targetGO.Value.transform.position - this.transform.position).magnitude;
+			if (!isCalculatingPath && Time.time > lastUpdatePath + updatePathDelay.Value)
+			{
+				Vector3 target = targetGO.Value.transform.position;
+				if (distance < minDistance.Value)
+					target = this.transform.position - (targetGO.Value.transform.position - this.transform.position).normalized * 100f;
+				Seeker seeker = controller.GetComponent<Seeker>();
+				isCalculatingPath = true;
+				seeker.StartPath(seeker.transform.position, target, ((Pathfinding.Path p) => {currentPath = p; currentPathIndex = 2;isCalculatingPath = false;lastUpdatePath = Time.time;}));
+			}
+			if (currentPath != null && currentPath.vectorPath.Count > currentPathIndex)
+			{
+				Vector3 nextTarget = currentPath.vectorPath[currentPathIndex];
+				Vector2 dif = nextTarget - this.transform.position;
+				while (dif.magnitude < 0.1f)
+				{
+					currentPathIndex++;
+					if (currentPathIndex >= currentPath.vectorPath.Count)
+					{
+						currentPath = null;
+						break;
+					}
+					nextTarget = currentPath.vectorPath[currentPathIndex];
+					dif = nextTarget - this.transform.position;
+				}
+				if (distance < minDistance.Value || distance > maxDistance.Value)
+					controller.velocity = (nextTarget - this.transform.position).normalized * speed.Value;
+				else
+					controller.velocity = Vector2.zero;
+			}
+
+//			Vector2 dif = targetGO.Value.transform.position - this.transform.position;
+//			float distance = dif.magnitude;
+//			if (distance < minDistance.Value)
+//				controller.velocity = -(targetGO.Value.transform.position - this.transform.position).normalized * speed.Value;
+//			else if (distance > maxDistance.Value)
+//				controller.velocity = (targetGO.Value.transform.position - this.transform.position).normalized * speed.Value;
+//			else
+//				controller.velocity = Vector2.zero;
 			return TaskStatus.Running;
 		}
 		return TaskStatus.Failure;
---
+++ Assets/Scripts/UpdatePathfindingGraph.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePathfindingGraph : MonoBehaviour {
+
+	public float timeBetweenScans = 1f;
+
+	float timeLastScan;
+
+	AstarPath _path;
+	AstarPath path
+	{
+		get
+		{
+			if (_path == null) _path = GetComponent<AstarPath>();
+			return _path;
+		}
+	}
+
+	void Start()
+	{
+		timeLastScan = Time.time;
+	}
+
+	void Update () 
+	{
+		if (Time.time > timeLastScan + timeBetweenScans)
+		{
+			path.Scan();
+			timeLastScan = Time.time;
+		}
+	}
+}
